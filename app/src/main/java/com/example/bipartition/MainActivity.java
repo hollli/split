bf523be55767fdcfb7152e7c4890fce45ae3deee
holli <619metal619@gmail.com>
2022-12-07 02:43:18 +0200

    JAVA SPLIT ANDROID SIZE/2

--- app/src/main/java/com/example/bipartition/MainActivity.java
+++ app/src/main/java/com/example/bipartition/MainActivity.java
@@ -17,21 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mButton = (Button) findViewById(R.id.button);
+        Button mButton = findViewById(R.id.button);
         mText = findViewById(R.id.editText2);
         next_text = findViewById(R.id.next_string);
 
         mButton.setOnClickListener(v -> {
-//        String.valueOf(mText.getText())
             String myLine = mText.getText().toString();
-
             int size = myLine.length();
-            int help = size/2+1;
-            String finalMyLine = myLine;
-            String result = finalMyLine.substring(0,help) +"\n༼ つ ◕_◕ ༽つ\n"+ finalMyLine.substring(help,size);
-
+            int halfSize = size/2+1;
+            String result = myLine.substring(0,halfSize) +"\n༼ つ ◕_◕ ༽つ\n"+ myLine.substring(halfSize,size);
             next_text.setText(result);
-//                myLine.substring(1,help) +" "+ myLine.substring(size)
         });
     }
 }
